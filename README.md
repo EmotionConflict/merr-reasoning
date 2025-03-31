b2f68ee66dedd0ad3db4706014c8e341f6ddb8e0
Charlie Chenyu Zhang <charlie.chenyu.zhang@gmail.com>
2025-03-31 00:23:50 -0400

    gpt40_mini_result

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # chenyu-llm-experiment
+
+## set up conda environment
+
+- `conda env create -f environment.yaml`
+- `conda activate how2ai-llm-experiment`
+
+## set up .env
+
+OPENAI_API_KEY=<your_key>
---
+++ constants.py
@@ -0,0 +1,24 @@
+# This is what Emotion-LLaMA uses as the system prompt.
+SYSTEM_PROMPT = (
+    "Please determine which emotion label in the video represents: happy, sad, neutral, angry, worried, surprise, fear, contempt, doubt. Answer the emotion label in lower-case."
+)
+
+SYSTEM_PROMPT_CHAIN_OF_THOUGHT = (
+    """
+    Let's think step by step.
+    Please determine which emotion label in the video represents: happy, sad, neutral, angry, worried, surprise, fear, contempt, doubt. Answer the emotion label in lower-case.
+    """
+)
+
+SYSTEM_PROMPT_TREE_OF_THOUGHT = (
+    """
+    Imagine three different experts are answering this question.
+    All experts will write down 1 step of their thinking,
+    then share it with the group.
+    Then all experts will go on to the next step, etc.
+    If any expert realises they're wrong at any point then they leave.
+    The question is...
+    
+    Please determine which emotion label in the video represents: happy, sad, neutral, angry, worried, surprise, fear, contempt, doubt. Answer the emotion label in lower-case.
+    """
+)
